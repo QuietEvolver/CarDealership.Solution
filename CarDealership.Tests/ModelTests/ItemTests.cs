f24aa28cda859bfefaa3da62ca602bde10a781c9
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-02-28 11:09:04 -0800

    working clear

---
+++ CarDealership.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,98 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using CarDealership.Models;
+using System;
+
+namespace CarDealership.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+
+      //Act
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Wagon";
+      string description02 = "Wagoneer";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void ClearAll_ClearsAllListItems_BlankItemList()
+    {
+      Item Item1 = new Item("2005 Honda Civic");
+      Item Item2 = new Item("1987 Toyota Camry");
+      List<Item> newList = new List<Item> { Item1, Item2 }; 
+      
+      Item.ClearAll();
+
+      Assert.AreEqual(0, Item.GetAll().Count);
+    }
+
+    
+  }
+}
---
+++ CarDealership/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
+using System.Collections.Generic;
+
+namespace CarDealership.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+  }
+}

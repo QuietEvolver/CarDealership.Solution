2b6a8e667acebdbefa9677b59a7d4a1b25f5bea8
Vera Weikel <vera.weikel@gmail.com>
2023-03-01 09:12:30 -0800

    It wil update home controller.

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,46 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using CarDealership.Models;
-using System.Collections.Generic;
 
-namespace CarDealership.Controllers
+namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
 
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
     }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
-
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
-    }
-
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
-
-  }
 }

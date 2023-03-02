ac44bbf7c42f3b82898823e3a85f77d5c34de29c
Vera Weikel <vera.weikel@gmail.com>
2023-03-01 18:49:07 -0800

    Category controller, Index/display GetAll().

--- CarDealership/Controllers/CategoriesController.cs
+++ CarDealership/Controllers/CategoriesController.cs
@@ -1,4 +1,19 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using CarDealership.Controllers;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
+
+
+namespace CarDealership.Controllers
+{
+  public class CategoriesController : Controller
+  {
+  // create an Index() route to display all Categorys
+  [HttpGet("/categories")]
+  public ActionResult Index()
+  {
+    List<Category> allCategories = Category.GetAll();
+    return View(allCategories);
+  }
+  }
+}
